2bfd92931f1d38e42511994decfc8396db306d29
dewey <mail@notmyhostna.me>
2012-09-20 23:08:04 +0200

    Now with Tax Classes and setStatus

--- set-group-prices-via-sku.php
+++ set-group-prices-via-sku.php
@@ -9,21 +9,25 @@ foreach ($collection as $product) {
 
 $productcount = count($collection);
 $changed = 0;
+$wouldchange = 0;
 
 # Select Product (fuzzy search)
-$needle   = '41000';
+$needle   = '10300';
 
 # Shows affected products
 $simulation = false;
 
 # New group price
-$price = 999;
+$price = 0.33;
+
+# Tax Class
+$taxclass = 1;
 
 for($k = 0; $k < $productcount; $k++) {
     $pos = strpos($result[$k]['sku'], $needle);
-
+    
     if ($pos !== false) {
-        $loadedproduct = Mage::getModel('catalog/product')->setStoreId(0)->load($result[$k]['entity_id']);
+        $loadedproduct = Mage::getModel('catalog/product')->setStoreId(0)->load($result[$k]['entity_id']);    
         if($simulation == false) {
             echo "✏ ";
             $loadedproduct->setData('group_price',array (
@@ -32,16 +36,27 @@ for($k = 0; $k < $productcount; $k++) {
                 "cust_group" => 3,
                 "price" => $price
             )));
-            $loadedproduct->save();
+            $loadedproduct->setTaxClassId($taxclass);
+            
+            # Deactivate all products, then run this script -> you know it worked if your products are activate when you are done.
+            $loadedproduct->setStatus(1);
             $changed++;
+            
+            try {
+            $loadedproduct->save();
+            }
+            catch (Exception $ex) {  
+                echo "Error!\n";
+            }
         }
-        echo "Set group price for ".$result[$k]['sku']." to ".$price." Euro ✔\n";
+        $wouldchange++;
+        echo "Set group price & taxclass for ".$result[$k]['sku']." to ".$price." Euro / Class ".$taxclass." ✔\n";
     }
 }
 
 echo "---------------------------------\n";
 echo "Productcount: ".$productcount."\n";
-echo "Changed:      ".$changed."\n";
+echo ($simulation == true ? "Would Change: ".$wouldchange."\n" : "Changed:      ".$changed."\n");
 echo "WebsiteID:    ".Mage::app()->getWebsite()->getId()."\n";
 echo "StoreID:      ".Mage::app()->getStore()->getId()."\n";
 echo "GroupID:      ".Mage::getSingleton('customer/session')->getCustomerGroupId()."\n";
